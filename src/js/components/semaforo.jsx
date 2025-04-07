33022a96b7fb205496c1d3be8a4d4b5a675bc8f2
Pere Pau Carrasco Quesada <perepaucarrasco@gmail.com>
2025-04-07 11:29:04 +0000

    my first commit

--- src/js/components/semaforo.jsx
+++ src/js/components/semaforo.jsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+import React, {useState} from "react";
 
-const Semaforo = () => {
-    const [selected, setSelected] = useState('')
+const semaforo = () => {
+    const  [selected, setSelected] = useState ("")
     const handleClick = (val) => {
         setSelected(val)
     }
     return (
-        <div className={Semaforo} id="Semaforo">
-            <div className={`${selected === 'luz-Roja' ? 'luz-Roja active' : 'luz-Roja inactive'}`} onClick={() => handleClick('luz-Roja')}></div>
-            <div className={`${selected === 'luz-Amarilla' ? 'luz-Amarilla active' : 'luz-Amarilla inactive'}`} onClick={() => handleClick('luz-Amarilla')}></div>
-            <div className={`${selected === 'luz-Verde' ? 'luz-Verde active' : 'luz-Verde inactive'}`} onClick={() => handleClick('luz-Verde')}></div>
+        <div className={"semaforo"} id="semaforo">
+            <div className= {`${selected === 'luz-roja' ? 'luz-roja active' : 'luz-roja inactive'}`} onClick={() => handleClick('luz-roja')}></div>
+            <div className={`${selected === 'luz-amarilla' ? 'luz-amarilla active' : 'luz-amarilla inactive'}`} onClick={() => handleClick('luz-amarilla')}></div>
+            <div className={`${selected === 'luz-verde' ? 'luz-verde active' : 'luz-verde inactive'}`} onClick={() => handleClick('luz-verde')}></div>
         </div>
+
     );
 };
 
-export default Semaforo;
+export default semaforo;
